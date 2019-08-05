d8d92f807433db39304ee9f4e30ce7a90185e552
pedrofrati <51917829+pedrofrati@users.noreply.github.com>
2019-08-05 12:17:05 -0300

    Início do layout da página de login

---
+++ login.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!-- Compiled and minified CSS -->
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    	<!-- Compiled and minified JavaScript -->
+    	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<title>Login | Pousada Elegance</title>
+		<style type="text/css">
+			body{
+				background-color: #FFF7D9;
+			}
+
+			 /* label focus color */
+			.input-field input[type=text]:focus + label {
+			    color: #fbc02d !important;
+			}
+
+			.input-field input[type=password]:focus + label {
+			    color: #fbc02d !important;
+			}
+			/* label underline focus color */
+			.input-field input[type=text]:focus {
+			    border-bottom: 1px solid #fbc02d !important;
+			    box-shadow: 0 1px 0 0 #fbc02d !important;
+			}
+			.input-field input[type=password]:focus {
+			    border-bottom: 1px solid #fbc02d !important;
+			    box-shadow: 0 1px 0 0 #fbc02d !important;
+			}
+		</style>
+	</head>
+	<body>
+		<center>
+			<nav class="grey darken-2">
+				<div class="nav-wrapper">
+					<i class="large material-icons">account_circle</i>
+				</div>			
+			</nav>
+			<br>
+			<b style="font-size: 24px;">LOGIN</b>
+			<div class="card-panel" style="width: 40%;">
+				<div class="row">
+				    <form class="col s12" method="post">
+				      	<div class="row">
+				        	<div class="input-field col s12">
+				          		<input id="cpf" type="text" class="validate" name="cpf" required="">
+				          		<label for="cpf">CPF</label>
+				        	</div>
+				    	</div>
+				    	<div class="row">
+				        	<div class="input-field col s12">
+				          		<input id="password" type="password" class="validate" name="senha" required="">
+				          		<label for="password">Senha</label>
+				        	</div>
+				    	</div>
+				    	<button class="btn waves-effect waves-light yellow darken-2" type="submit" name="action">Entrar
+    						<i class="material-icons right">input</i>
+  						</button>
+					</form>
+				</div>
+	  		</div>
+	  		<div class="card-panel" style="width: 40%;">
+	  			Novo por aqui? <a href="#">Crie uma conta</a>
+	  		</div>
+		</center>
+	</body>
+</html>
+
+<?php
+	if(isset($_POST['cpf']) && isset($_POST['senha'])){
+		$cpf = $_POST['cpf'];
+		$senha = $_POST['senha'];
+		$sql = "SELECT * from tb_cliente where nr_cpf = '$cpf' and ds_senha = '$senha'";
+		$result = $mysqli->query($sql);
+		if($result->num_rows > 0){
+			$row = $result->fetch_object();
+					
+			}
+			else{
+				
+			}
+		}
+	?>
