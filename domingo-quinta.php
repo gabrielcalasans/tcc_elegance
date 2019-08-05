814a3230a8c7ec9059cb3cf38f3da63e91413444
Gabriel Calasans <48914341+gabrielcalasans@users.noreply.github.com>
2019-08-05 12:32:05 -0300

    Adição dos futuros selects dos dias

---
+++ domingo-quinta.php
@@ -0,0 +1,14 @@
+<?php 
+$consultapreco="SELECT vl_quarto FROM tb_quarto";
+$resultado = $mysqli->query($consultapreco);
+while($linha = $resultado->fetch_object())
+		{
+							
+		}
+
+
+
+
+
+
+?>
