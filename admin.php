1567f5bb5614e3e2f1fec6b33754758f7ac15e3d
Carlos Alberto Lima <51917436+yTzVortex@users.noreply.github.com>
2019-08-06 09:28:11 -0300

    Pagina admin atualizada

--- admin.php
+++ admin.php
@@ -1,7 +1,39 @@
 <?php include('header.php'); ?>
-	<title>Login Administrador</title>
+	<title>Login administrador | Pousada Elegance<</title>
+		<style type="text/css">
+		body{
+				background-color: #5C677D;
+			}
+			 /* label focus color */
+			.input-field input[type=text]:focus + label {
+			    color: #fbc02d !important;
+			}
+
+			.input-field input[type=password]:focus + label {
+			    color: #fbc02d !important;
+			}
+			/* label underline focus color */
+			.input-field input[type=text]:focus {
+			    border-bottom: 1px solid #fbc02d !important;
+			    box-shadow: 0 1px 0 0 #fbc02d !important;
+			}
+			.input-field input[type=password]:focus {
+			    border-bottom: 1px solid #fbc02d !important;
+			    box-shadow: 0 1px 0 0 #fbc02d !important;
+			}
+			.icon{
+				width: 20%;
+			}
+		</style>
 </head>
 <body>
+	<center>
+		<nav class="black darken-2">
+				<div class="nav-wrapper">
+					<i class="large material-icons">account_circle</i>
+				</div>			
+		</nav>
+	</center>
 	<?php
 	include('conn.php');
 
@@ -31,11 +63,29 @@
 		}
 	}
 	?>
-	<form method="post">
-		Login:<input type="text" name="login"></p>
-		Senha:<input type="text" name="senha">
-		<input type="submit">
-	</form>
 
+	<center>
+	<b style="font-size: 24px;">ADMIN</b>
+		<div class="card-panel" style="width: 30%;">
+			<div class="row">
+				<form class="col s12" method="post">
+					<div class="row">
+					<div class="row">
+				        	<div class="input-field col s12">
+				          		<input id="cpf" type="text" class="validate" name="login" required="">
+				          		<label for="cpf">Login</label>
+				        	</div>
+				    	</div>
+				    	<div class="row">
+				        	<div class="input-field col s12">
+				          		<input id="password" type="password" class="validate" name="senha" required="">
+				          		<label for="password">Senha</label>
+				        	</div>
+				    	</div>
+					<button class="btn waves-effect waves-light green darken-2" type="submit" name="action">CONFIRMAR</button>
+				</form>
+			</div>
+		</div>
+	</center>
 </body>
 <?php include('footer.php'); ?>
