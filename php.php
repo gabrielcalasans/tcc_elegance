8003c4b6288ec2eb6edacd2c39f68e43f80879ac
Carlos Alberto Lima <51917436+yTzVortex@users.noreply.github.com>
2019-08-12 12:19:35 -0300

    Página php atualizada

--- php.php
+++ php.php
@@ -5,9 +5,10 @@
     inner join tb_estado est on (cid.id_estado = est.cd_estado)
     where est.cd_estado = '$estado'
     order by cid.nm_cidade asc";
-    $result = $mysqli->query($sql);
-    echo "<option value='' disabled selected>Selecione...</option>";
+    $result = $mysqli->query($sql); 
+    $cidades = "<option value='' disabled selected>Selecione...</option>";
     while($row = $result->fetch_object()){
-    	echo "<option name='cidade' value='".$row->codcidade."'>".$row->cidade."</option>";
+    	$cidades .= "<option name='cidade' value='".$row->codcidade."'>".$row->cidade."</option>";
     }
+    echo $cidades;
 ?>
