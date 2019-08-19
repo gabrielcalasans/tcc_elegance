64760040a69ad59b7e97b43af4ed4cb4f683496f
Gabriel Calasans <48914341+gabrielcalasans@users.noreply.github.com>
2019-08-19 09:38:44 -0300

    Criação da Página de Estatísticas

---
+++ estatisticas.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <?php
+          include('conn.php');
+          date_default_timezone_set('UTC');
+          date_default_timezone_set('America/Sao_paulo');
+      ?>
+        <meta charset="utf-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		<title>Estatística | Pousada Hospedagem Elegance</title>
+		<style type="text/css">
+
+		</style>
+	</head>
+  <?php
+
+    //CONSULTA QUANTIDADE DE RESERVAS
+  $consulta = "SELECT count(cd_reserva) AS total, sum(vl_reserva) AS vltotal FROM tb_reserva";
+  $resultado = $mysqli->query($consulta);
+  while($dados = $resultado->fetch_object())
+  {
+    $reservas_totais = $dados->total;
+    $lucro_total = $dados->vltotal;
+  }
+
+  //CONSULTA Lucro e Quantidade de Reservas
+  $valor= "SELECT count(cd_reserva) as reserva , sum(vl_reserva) as lucro
+           FROM tb_reserva
+           WHERE MONTH(dthr_registro)
+           BETWEEN MONTH( NOW() ) -1
+           AND MONTH( NOW( ) )";
+
+  $resultado = $mysqli->query($valor);
+  while($dados = $resultado->fetch_object())
+  {
+
+    $lucro_mes = $dados->lucro;
+    $reservas_mes = $dados->reserva;
+
+  }
+
+
+
+
+
+  ?>
+	<body>
+    Total de reservas:
+    <?php
+    echo $reservas_totais;
+
+		?>
+    Reservas no último mês:
+    <?php
+    echo $reservas_mes;
+
+    ?>
+    Lucro do último mês:
+    <?php
+    echo $lucro_mes;
+    ?>
+
+	</body>
+
+</html>
