59686ff7aeb54d903b6e80100ed72cfa5149668d
Igor Oliveira <45038664+igoretec@users.noreply.github.com>
2019-08-28 09:42:15 -0300

    Atualizando

--- contato.php
+++ contato.php
@@ -1,7 +1,6 @@
 <?php include('header.php'); date_default_timezone_set('UTC'); ?>
 	<title>Contato | Hospedagem Elegance</title>
 	</header>
-
 	<style type="text/css">
             body{
                 background-color: #FFF7D9;
@@ -135,7 +134,7 @@
                 </ul>
             </div>
         </nav>
-	      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3639.6593140516566!2d-46.78527668536784!3d-24.18367809075139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d1d59cf263e053%3A0x81ea880496da1235!2sHospedagem%20Elegance!5e0!3m2!1spt-BR!2sbr!4v1566819445690!5m2!1spt-BR!2sbr" width="1343" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+	      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3639.6593140516566!2d-46.78527668536784!3d-24.18367809075139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d1d59cf263e053%3A0x81ea880496da1235!2sHospedagem%20Elegance!5e0!3m2!1spt-BR!2sbr!4v1566819445690!5m2!1spt-BR!2sbr" width="2000" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 	    <div class="container">
 	      <div class="row">
       		<div class="col s6">
@@ -156,6 +155,18 @@
 						<div class="card-panel" style="width: 100%;">
 							<div class="row">
 								<center><h5 id="fale"><b>Fale Conosco:</b></h5></center>
+                                <?php 
+                                    if(!empty($_SESSION['cliente'])){
+                                        echo "<div class='input-field col s12'>
+                                                <p align='center'><label for='m1'>
+                                                    <input class='with-gap' id='m1' type='radio' name='tipo' value='1' checked><span>Mensagem</span>
+                                                </label>
+                                                <label for='m2'>
+                                                    <input class='with-gap' id='m2' type='radio' name='tipo' value='2'><span>Depoimento</span>
+                                                </label></p>
+                                            </div>";
+                                    }
+                                ?> 
 								<div class="input-field col s6">
 									<input id="nome" type="text" name="nome" required="" class="validate"><label for="nome">Nome</label>
 								</div>
@@ -165,9 +176,23 @@
 								<div class="input-field col s6">
 									<input id="telefone" type="text" name="telefone" required="" class="validate"><label for="telefone">Telefone</label>
 								</div>
-								<div class="input-field col s6">
-									<input id="email" type="text" name="email" required="" class="validate"><label for="email">E-mail</label>
-								</div>
+                                <?php 
+                                    if(!empty($_SESSION['cliente'])){
+                                        $sql = "SELECT * from tb_cliente where cd_cliente =".$_SESSION['cliente'];
+                                        $result = $mysqli->query($sql);
+                                        $row = $result->fetch_object();
+                                        echo '<div class="input-field col s6">
+                                                <input id="email" type="text" name="email" required="" class="validate" value="'.$row->ds_email.'"><label for="email">E-mail</label>
+                                            </div>';
+                                    }
+                                    else{
+                                        echo '<div class="input-field col s6">
+                                    <input id="email" type="text" name="email" required="" class="validate"><label for="email">E-mail</label>
+                                </div>';
+                                    }
+                                ?>
+                                
+								
 								<div class="input-field col s12">
 						            <textarea id="digite" class="materialize-textarea" name="msg" data-length="200" maxlength="200"></textarea>
 						            <label for="digite">Digite sua mensagem</label>
@@ -183,36 +208,40 @@
 				</div>
 			</div>
 			<?php
-				$nome = $_POST['nome'];
-				$email = $_POST['email'];
-				$telefone = $_POST['telefone'];
-				$assunto = $_POST['assunto'];
-				$mensagem = $_POST['msg'];
-				$data_envio = date('d/m/Y');
-				$hora_envio = date('H:i:s');
-						// emails para quem será enviado o formulário
-				$emailenviar = "pousadaelegance@gmail.com";
-				$destino = $emailenviar;
+                if(isset($_POST['nome'])){
+                    $nome = $_POST['nome'];
+                    $email = $_POST['email'];
+                    $telefone = $_POST['telefone'];
+                    $assunto = $_POST['assunto'];
+                    $mensagem = $_POST['msg'];
+                    $data_envio = date('d/m/Y');
+                    $hora_envio = date('H:i:s');
+                            // emails para quem será enviado o formulário
+                    $emailenviar = "pousadaelegance@gmail.com";
+                    $destino = $emailenviar;
 
-				// É necessário indicar que o formato do e-mail é html
-				$headers  = 'MIME-Version: 1.0' . "\r\n";
-				  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-				  $headers .= 'From: $nome <$email>';
-				//$headers .= "Bcc: $EmailPadrao\r\n";
+                    // É necessário indicar que o formato do e-mail é html
+                    $headers  = 'MIME-Version: 1.0' . "\r\n";
+                    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                    $headers .= 'From: $nome <$email>';
+                    //$headers .= "Bcc: $EmailPadrao\r\n";
 
-				$enviaremail = mail($destino, $assunto,  $headers);
-				if($enviaremail){
-				$mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-				echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
-				} else {
-				$mgm = "ERRO AO ENVIAR E-MAIL!";
-				echo "";
-				}
+                    $enviaremail = mail($destino, $assunto,  $headers);
+                    if($enviaremail){
+                    $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+                    echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+                    } else {
+                    $mgm = "ERRO AO ENVIAR E-MAIL!";
+                    echo "";
+                    }
+                }
+				
 			?>
 </body>
-<script type="text/javascript">
-	$(document).ready(function() {
-    $('input#input_text, textarea#digite').characterCounter();
-  });
+<script>
+	$(document).ready(function(){
+        $('select').formSelect();
+        $('input#input_text, textarea#digite').characterCounter();
+    });
 </script>
 <?php include('footer.php'); ?>
--- footer.php
+++ footer.php
@@ -31,7 +31,6 @@
             </div>
         </footer>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	</body>
 </html>
 
