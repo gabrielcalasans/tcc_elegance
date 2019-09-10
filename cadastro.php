ba95992830d717fc1c22052061d6f85b7121d620
pedrofrati <51917829+pedrofrati@users.noreply.github.com>
2019-09-10 09:08:37 -0300

    Organizando o código

--- cadastro.php
+++ cadastro.php
@@ -2,7 +2,6 @@
 <html>
     <head>
         <meta charset="utf-8">
-
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>
     	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -54,115 +53,114 @@
 	</head>
 	<body>
 		<center>
-		<nav class="grey darken-2">
-			<div class="nav-wrapper">
-				<a href="index.php"><img id="logo" src="images/logotipo2.png"></a>
-			</div>			
-		</nav>
-		<?php
-			include('conn.php');
-			date_default_timezone_set('UTC');
-		?>
-		</br>
+			<nav class="grey darken-2">
+				<div class="nav-wrapper">
+					<a href="index.php"><img id="logo" src="images/logotipo2.png"></a>
+				</div>			
+			</nav>
+			<?php
+				include('conn.php');
+				date_default_timezone_set('UTC');
+			?>
+			</br>
 			<b style="font-size: 24px;">CADASTRO</b>
-		<p>
-		<form class="col s12" method="post">
-			<div class="card-panel" style="width: 60%;">
-				<div class="row">
-					<div class="input-field col s6">
-						<input id="nome" type="text" name="nome" required="" class="validate"><label for="nome">Nome</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="sobrenome" type="text" name="sobrenome" required="" class="validate"><label for="sobrenome">Sobrenome</label>
-					</div>
-					<div class="input-field col s12">	
-						Gênero:<p>
-						<?php
-							$sql = "SELECT * from tb_genero";
-							$result = $mysqli->query($sql);
-							while($row = $result->fetch_object()){
-								echo "<label for='".$row->cd_genero."'>
-										<input class='with-gap' id='".$row->cd_genero."' type='radio' name='genero' value='".$row->cd_genero."' required=''><span>".$row->nm_genero."</span>
-									</label>";
-							}
-						?>	
-				    	</p>
-					</div>
-					<div class="input-field col s6">	
-						<input id="cpf" type="text" name="cpf" required="" class="validate"><label for="cpf">CPF</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="email" type="email" name="email" required="" class="validate"><label for="email">E-mail</label>
-					</div>
-					
-					<div class="input-field col s6">		
-						<input id="celular" type="text" name="celular" required="" class="validate"><label for="celular">Celular</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="tel" type="text" name="telefone"><label for="tel">Telefone (opcional)</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="rg" type="text" name="rg" required="" class="validate"><label for="rg">RG</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="orgao" type="text" name="orgao" required="" class="validate"><label for="orgao">Orgão Expedidor</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="nacionalidade" type="text" name="nacionalidade" required="" class="validate"><label for="nacionalidade">Nacionalidade</label>
-					</div>
-					<div class="input-field col s6">
-						<input id="data" type="text" class="datepicker" name="datanasc" required="" class="validate"><label for="data">Data de Nascimento</label>
-					</div>
-					<div class="input-field col s6">
-						<select name="profissao">
-							<option value="" disabled selected><label for="profissao">Profissão</label></option>
+			<form class="col s12" method="post">
+				<div class="card-panel" style="width: 60%;">
+					<div class="row">
+						<div class="input-field col s6">
+							<input id="nome" type="text" name="nome" required="" class="validate"><label for="nome">Nome</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="sobrenome" type="text" name="sobrenome" required="" class="validate"><label for="sobrenome">Sobrenome</label>
+						</div>
+						<div class="input-field col s12">	
+							Gênero:<p>
 							<?php
-								$sql = "SELECT * from tb_profissao order by nm_profissao asc";
+								$sql = "SELECT * from tb_genero";
 								$result = $mysqli->query($sql);
 								while($row = $result->fetch_object()){
-									echo "<option value='".$row->cd_profissao."'>".$row->nm_profissao."</option>";
+									echo "<label for='".$row->cd_genero."'>
+											<input class='with-gap' id='".$row->cd_genero."' type='radio' name='genero' value='".$row->cd_genero."' required=''><span>".$row->nm_genero."</span>
+										</label>";
 								}
-							?>
-						</select>
-					</div>
+							?>	
+					    	</p>
+						</div>
+						<div class="input-field col s6">	
+							<input id="cpf" type="text" name="cpf" required="" class="validate"><label for="cpf">CPF</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="email" type="email" name="email" required="" class="validate"><label for="email">E-mail</label>
+						</div>
+						
+						<div class="input-field col s6">		
+							<input id="celular" type="text" name="celular" required="" class="validate"><label for="celular">Celular</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="tel" type="text" name="telefone"><label for="tel">Telefone (opcional)</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="rg" type="text" name="rg" required="" class="validate"><label for="rg">RG</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="orgao" type="text" name="orgao" required="" class="validate"><label for="orgao">Orgão Expedidor</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="nacionalidade" type="text" name="nacionalidade" required="" class="validate"><label for="nacionalidade">Nacionalidade</label>
+						</div>
+						<div class="input-field col s6">
+							<input id="data" type="text" class="datepicker" name="datanasc" required="" class="validate"><label for="data">Data de Nascimento</label>
+						</div>
+						<div class="input-field col s6">
+							<select name="profissao">
+								<option value="" disabled selected><label for="profissao">Profissão</label></option>
+								<?php
+									$sql = "SELECT * from tb_profissao order by nm_profissao asc";
+									$result = $mysqli->query($sql);
+									while($row = $result->fetch_object()){
+										echo "<option value='".$row->cd_profissao."'>".$row->nm_profissao."</option>";
+									}
+								?>
+							</select>
+						</div>
 						<div class="input-field col s6"> 
 							<input id="outra" type="text" name="outra"><label for="outra">Outra</label></p>
-					</div>
-				</div>
-				</div>
-					<div class="card-panel" style="width: 60%;">
-						<div class="row">
-							<div class="input-field col s8">
-								<input id="endereco" type="text" name="endereco" required="" class="validate"><label for="endereco">Endereço</label>
-							</div>
-							<div class="input-field col s4">
-								 <input type="text" id="numero" name="numero" required="" class="validate"><label for="numero">Número</label>
-							</div>
-							<div class="input-field col s6">
-								<select id="estado" name="estado" required="">
-									<option value="" disabled selected>Estado</option>
-									<?php
-										$sql = "SELECT * from tb_estado order by nm_estado asc";
-										$result = $mysqli->query($sql);
-										while($row = $result->fetch_object()){
-											echo "<option value='".$row->cd_estado."'>".$row->nm_estado."</option>";
-										}
-									?>
-								</select>
-							</div>
-							<div class="input-field col s6">
-								<select id="cidade" name="cidade" required="">
-									<option value="" disabled selected>Cidade</option>
-								</select></p>
-							</div>
-							<div class="input-field col s6">
-								<input id="bairro" type="text" name="bairro" required="" class="validate"><label for="bairro">Bairro</label></p>
-							</div>
-							<div class="input-field col s6">
-								<input id="cep" type="text" name="cep" required="" class="validate"><label for="cep">CEP</label></p>
-							</div>
 						</div>
 					</div>
+				</div>
+				<div class="card-panel" style="width: 60%;">
+					<div class="row">
+						<div class="input-field col s8">
+							<input id="endereco" type="text" name="endereco" required="" class="validate"><label for="endereco">Endereço</label>
+						</div>
+						<div class="input-field col s4">
+							<input type="text" id="numero" name="numero" required="" class="validate"><label for="numero">Número</label>
+						</div>
+						<div class="input-field col s6">
+							<select id="estado" name="estado" required="">
+								<option value="" disabled selected>Estado</option>
+								<?php
+									$sql = "SELECT * from tb_estado order by nm_estado asc";
+									$result = $mysqli->query($sql);
+									while($row = $result->fetch_object()){
+										echo "<option value='".$row->cd_estado."'>".$row->nm_estado."</option>";
+									}
+								?>
+							</select>
+						</div>
+						<div class="input-field col s6">
+							<select id="cidade" name="cidade" required="">
+								<option value="" disabled selected>Cidade</option>
+							</select>
+						</div>
+						<div class="input-field col s6">
+							<input id="bairro" type="text" name="bairro" required="" class="validate"><label for="bairro">Bairro</label></p>
+						</div>
+						<div class="input-field col s6">
+							<input id="cep" type="text" name="cep" required="" class="validate"><label for="cep">CEP</label></p>
+						</div>
+					</div>
+				</div>
 				<div class="card-panel" style="width: 60%;">
 					<div class="row">
 						<div class="input-field col s6">
@@ -171,20 +169,20 @@
 						<div class="input-field col s6">
 							<input id="s2" type="password" name="senha1" required="" class="validate"><label for="s2">Confirmar senha</label><span class="senhas"></span></p>
 						</div>
-							<label for="termos">
-								<div class="input-field col s12">
-									<input id="termos" type="checkbox" name="termos" required=""  class="filled-in checkbox-yellow"><span> Li os <a href="#">termos de uso</a> e aceito todas as condições.</span>
-								</div>
-							</label>
+						<label for="termos">
+							<div class="input-field col s12">
+								<input id="termos" type="checkbox" name="termos" required=""  class="filled-in checkbox-yellow"><span> Li os <a href="#">termos de uso</a> e aceito todas as condições.</span>
+							</div>
+						</label>
 					</div>
 				</div>
-			<button class="btn waves-effect waves-light green accent-4" type="submit" id="cadastrar" name="action">
-				Cadastrar
-			</button>
-			<button class="btn waves-effect waves-light red" type="reset" id="limpar">
-				Limpar
-			</button>
-		</form>
+				<button class="btn waves-effect waves-light green accent-4" type="submit" id="cadastrar" name="action">
+					Cadastrar
+				</button>
+				<button class="btn waves-effect waves-light red" type="reset" id="limpar">
+					Limpar
+				</button>
+			</form>
 		</center>
 		<?php
 			if(isset($_POST['nome']) && isset($_POST['endereco'])){
