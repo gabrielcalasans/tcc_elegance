831f4cfcc2c1d0803cbb4473b5ec822c376c1bfe
Igor Oliveira <45038664+igoretec@users.noreply.github.com>
2019-09-19 08:39:34 -0300

    atualizando

--- painel_admin.php
+++ painel_admin.php
@@ -7,7 +7,8 @@
 <h3>Funcionário: <?php echo $nmfuncionario; ?></h3>
 <a href="ver_reserva.php"><button>Visualizar reservas</button></a><p>
 <a href="ver_quarto.php"><button>Visualizar quartos</button></a><p>
-<a href="estatisticas.php"><button>Visualizar estatísticas</button></a>
+<a href="estatisticas.php"><button>Visualizar estatísticas</button></a><p>
+<a href="addfotos.php"><button>Adicionar fotos</button></a>
 
 <?php 
 
