4a7c77efdd58502130235ece715597630817d3ce
Igor Oliveira <45038664+igoretec@users.noreply.github.com>
2019-09-23 12:20:39 -0300

    Atualizando

--- cliente.php
+++ cliente.php
@@ -8,22 +8,10 @@
             body{
             	background-color: #FFF7D9;
             }
-            
-            #panel{
-                transition: 0.4s;
-                width: 80%;
-            }
-
-            #panel:hover{
-                box-shadow: 0 6px 8px 0 rgba(0,0,0,0.24), 0 9px 40px 0 rgba(0,0,0,0.19);
-            }
-
-            .promo-caption{
-                font-size: 1.2rem;
-                font-weight: 500;
-                margin-top: 5px;
-            	margin-bottom: 0;
-            }
+        #avatar{
+        	width: 120px;
+        	height: 120px;
+        }
 	</style>
 	<?php
 		include('conn.php');
@@ -54,8 +42,8 @@
                
             </div>
             <div class="modal-footer">
-              <center><a href="cliente.php?id=0" title="Sim" class="btn modal-close waves-effect waves-light green accent-4">Sim</a>
-              <a href="#!" title="Não" class="btn modal-close waves-effect waves-light red">Não</a></center>
+              <center><a href="cliente.php?id=0" title="Sim" class="btn green modal-close waves-effect waves-green">Sim</a>
+              <a href="#!" title="Não" class="btn red modal-close waves-effect waves-green">Não</a></center>
             </div>
         </div>
 		<nav class="grey darken-2">
@@ -73,38 +61,93 @@
             </div>
 	    </nav>
 		<div class="container">
-	        <div class="row">
-	        	<div class="card-panel col s12">
-		    	<center>
-				    <div class="col s6">
-				    	<a href="escolhaquarto.php"><div id="panel" class="card-panel yellow darken-3">
-		                    <span>
-		                        <div class="center promo promo-example">
-		                            <i class="large material-icons">add_box</i>
-		                            <br>
-		                            <p class="promo-caption">Criar reserva</p>  
-		                        </div>
-		                    </span>
-		                </div></a>
-				    </div>
-				    <div class="col s6">
-				    	<a href="#"><div id="panel" class="card-panel yellow darken-3">
-		                    <span>
-		                        <div class="center promo promo-example">
-		                            <i class="large material-icons">remove_red_eye</i>
-		                            <br>
-		                            <p class="promo-caption">Visualizar reserva</p>  
-		                        </div>
-		                    </span>
-		                </div></a>
-				    </div>
-		     	</center>
-		        </div>
+			<?php 
+				$sql = "SELECT *
+						from tb_endereco ende
+						inner join tb_cliente cli on ( ende.cd_endereco = cli.id_endereco )
+						inner join tb_cidade cid on ( cid.cd_cidade = ende.id_cidade )
+						inner join tb_estado est on ( est.cd_estado = cid.id_estado )
+						where cli.cd_cliente = ".$_SESSION['cliente']
+						;
+				$result = $mysqli->query($sql);
+				$row = $result->fetch_object();
+			?>
+	        <div class="row card-panel">
+	        	<div align="center" class="col s12">
+					<img id="avatar" src="<?php echo $row->ds_avatar; ?>">
+				</div>
+				<div id="edit" align="center" class="col s12">
+					 <button id="editar" class="btn-small waves-effect waves-light orange darken-2" title="Editar">Editar<i class='material-icons right'>edit</i></button>
+				</div>
+				<div id="procurar" align="center" class="col s12">
+					<label>Edite sua foto de perfil</label>
+	               <div class = "file-field input-field">
+	                  <div class = "btn-small waves-effect waves-light orange darken-2">
+	                     <span>Procurar fotos<i class='material-icons right'>search</i></span>
+	                     <input type="file" id="img" name="img[]"/>
+	                  </div>
+	                  <div class = "file-path-wrapper">
+	                     <input id="img2" class = "file-path" type = "text" placeholder = "Carregue sua foto" />
+	                  </div>
+	               </div> 
+				</div>
+				<br>
+				<div class="input-field col s6">
+					<input class="alterar" id="nome" type="text" name="nome" readonly value="<?php echo $row->nm_cliente ?>"><label for="nome">Nome</label></p>
+				</div>
+	        	<div class="input-field col s6">
+					<input class="alterar" id="sobrenome" type="text" name="sobrenome" readonly value="<?php echo $row->nm_sobrenome ?>"><label for="sobrenome">Sobrenome</label></p>
+				</div>
+				<div class="input-field col s12" align="center">
+					Gênero:<p>
+							<?php
+								$sql1 = "SELECT * from tb_genero";
+								$result1 = $mysqli->query($sql1);
+								while($row1 = $result1->fetch_object()){
+									if ($row1->cd_genero == $row->id_genero){
+										echo "<label for='".$row1->cd_genero."'>
+											<input class='radioal with-gap' id='".$row1->cd_genero."' type='radio' name='genero' value='".$row1->cd_genero."' required='' checked disabled><span>".$row1->nm_genero."</span>
+										</label>";
+									}
+									else{
+										echo "<label for='".$row1->cd_genero."'>
+											<input class='radioal with-gap' id='".$row1->cd_genero."' type='radio' name='genero' value='".$row1->cd_genero."' required='' disabled><span>".$row1->nm_genero."</span>
+										</label>";
+									}
+								}
+							?>	
+					    	</p>
+				</div>
+	        	<div class="input-field col s4">
+					<input class="alterar" id="cpf" type="text" name="cpf" readonly value="<?php echo $row->nr_cpf; ?>"><label for="cpf">CPF</label></p>
+				</div>
+	        	<div class="input-field col s4">
+					<input class="alterar" id="rg" type="text" name="rg" readonly value="<?php echo $row->nr_rg ?>"><label for="rg">RG</label></p>
+				</div>
+				<div class="input-field col s4">
+					<input class="alterar" id="orgao" type="text" name="orgao" readonly value="<?php echo $row->ds_orgao ?>"><label for="orgao">Órgão de expedição</label></p>
+				</div>
+				<div class="input-field col s6">
+					<input class="alterar" id="cell" type="text" name="cell" readonly value="<?php echo $row->nr_celular ?>"><label for="cell">Celular</label></p>
+				</div>
+				<div class="input-field col s6 ">
+					<input class="alterar" id="tel" type="text" name="tel" readonly value="<?php echo $row->nr_telefone ?>"><label for="tel">Telefone</label></p>
+				</div>
 	        </div>
+	        <button id="alterar" class="btn-small red darken-2" title="Editar">Alterar dados<i class='material-icons right'>edit</i></button>
 		</div>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 		<script>
             $(document).ready(function(){
+            	$("#procurar").hide();
+            	$("#editar").click(function(){
+            		$("#procurar").show();
+            		$("#edit").hide();
+            	});
+            	$("#alterar").click(function(){
+            		$(".alterar").removeAttr('readonly');
+            		$(".radioal").removeAttr('disabled');
+            	});
                 $('.modal').modal();
             });
         </script>
