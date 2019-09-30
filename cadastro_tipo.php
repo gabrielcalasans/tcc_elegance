697c4112c34c0ae33618d5a3937870a105766b1c
Gabriel Calasans <48914341+gabrielcalasans@users.noreply.github.com>
2019-09-30 12:32:03 -0300

    Alteração 30-09
    Estilização e programação

--- cadastro_tipo.php
+++ cadastro_tipo.php
@@ -1,50 +1,130 @@
 <meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 <?php include('conn.php');
 	  date_default_timezone_set('America/Sao_paulo');
 	  //adicionar imagem
  ?>
+ <?php include('header.php'); ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <style type="text/css">
 	#lista
 	{
-		width: 100%;
-		height: 300px;
+		width: 50%;
+		height: 70.5%;
 		position: relative;
 	}
 	fieldset
 	{
 
 		float: left;
-		width: 70%;
-		height: 300px;
+		width: 60%;
+		height:75.5%;
 		padding: 10px;
 	}
 	#listagem
 	{
-		width: 300px;
-		height: 300px;
+		width: 40%;
+		height:70.5%;
 	}
+	#func
+	{
+		font-weight: 350;
+		color:white;
+	}	
+	#func:active
+	{
+		background-color:#9ccc65;
+	}
+	#imgespaco
+	{
+		width: 100%;
+		height:100%;
+	}
+	#imgcolocada
+	{
+		width: 100%;
+		height: 100%;
+	}
+	#imagem
+	{
+		width: 20%;
+		height: 30%;
+		position: relative;
+		margin-top:-6%;
+	}
+
+
+	
+	
+	
 </style>
 
 <form method="POST" enctype="multipart/form-data">
-	<fieldset>
-		Nome do Tipo: <input type="text" required name="tipo"><br>
-		Contém: <input type="text" id="descricao"> <a class="func">Adicionar</a><p>	
-		Valor R$:<input type="number" required id="dinheiro" name="dinheiro" class="dinheiro form-control" style="display:inline-block" /><br>
-		Imagem: <input type="file" name="imagem" accept="image/x-png,image/gif,image/jpeg" /><p>
-		<div id="img"></div>
-		<input type="submit" name="Enviar">
+	<fieldset id="formulario">
+		Nome do Tipo: <br>
+		<label>Adicione o nome do tipo  </label>
+		<input type="text" required name="tipo"><p>
+		Contém: <br>
+		<label>Adicione o que o tipo de quarto oferece  </label>
+		<div class="row">
+			<input class="col s2" type="text" id="descricao"> <a  class="waves-effect waves-light btn-small" id="func">Adicionar à descrição</a><p>	
+		</div>
+		Valor R$:<br>
+		<label>Adicione o valor do tipo do quarto</label>
+		<input type="number" required id="dinheiro" name="dinheiro" class="dinheiro form-control" style="display:inline-block" /><p>
+		Imagem:<br>
+		<label>Adicione fotos na galeria</label>
+	               <div class = "file-field input-field">
+	                  <div class = "btn-small waves-effect waves-light yellow darken-2">
+	                     <span>Procurar fotos<i class='material-icons right'>search</i></span>
+	                     <input type="file" id="img" name="imagem" accept="image/x-png,image/gif,image/jpeg"  />
+	                  </div>
+	                  
+		              
+	     <br><br><br><p>            
+
+ <button class="btn waves-effect waves-light" type="submit" name="action">Enviar
+    <i class="material-icons right">send</i>
+  </button>
+
 	</fieldset>
-	 <fieldset id="listagem"><textarea id="lista"></textarea></fieldset>
+
+
+	 <fieldset id="listagem">Descrição:<br>
+	 	<label for="exampleFormControlTextarea1">Adicione ou edite a descrição aqui</label>
+	 		<textarea id="lista" class="form-control" name="lista" id="exampleFormControlTextarea1" rows="3"></textarea>
+	 	</fieldset>
+
+	 	<fieldset id="imagem"><div id="imgespaco"></div></fieldset> 
+
 </form>
 		<script>
 			$(document).ready(function(){
 
-				$('.func').click(function(){
-					$('#lista').append('•'+$('#descricao').val()+'<br> ' );
+				$('#func').click(function(){
+					$('#lista').append('•'+$('#descricao').val()+'<br> ' );				
+					
 					
 				});
+
+			$('input[type="file"]').on('change', function() {
+			  var files = this.files;
+			  $(files).each(function(index, file) {
+			    // Still don't know why you want this...
+			    var fakepath = 'C:\\fakepath\\';
+			    $('#imgespaco').append('' +
+			      // build  a fake path string for each File
+			      '<p class="path"></p>' +
+			      // all that is really needed to display the image
+			      '<img id="imgcolocada" src="'+URL.createObjectURL(file)+'">' +
+			     '');
+			    $('#imgcolocada').attr('class', 'form-control');
+			  });
+			});
+
 
 
 			});
@@ -53,7 +133,7 @@
 if(isset($_POST['tipo']))
 {
 	$nome = $_POST['tipo'];
-	$descricao = $_POST['descricao'];
+	$descricao = $_POST['lista'];
 	$valor = $_POST['dinheiro'];	
 	if(isset($_FILES['imagem']))
 		{
@@ -75,7 +155,8 @@ if(isset($_POST['tipo']))
 		}
 	else
 		{
-			echo "<script type='text/javascript'>alert('Tipo de Quarto Cadastrado Com Sucesso!!'); window.location.href='cadastro_tipo.php';</script>";
+			echo "<script>alert('Cadastrado com sucesso!');</script>";
+			echo "<script>window.location.replace('cadastro_tipo.php');</script>";
 		}
 
 }
