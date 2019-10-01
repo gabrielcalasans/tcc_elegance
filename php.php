d221d3eab586e08f51a3ac21d4f10ce88434b245
Igor Oliveira <45038664+igoretec@users.noreply.github.com>
2019-10-01 09:40:19 -0300

    atualizando

--- php.php
+++ php.php
@@ -1,14 +1,27 @@
 <?php
 	include('conn.php');
-	$estado = $_POST['estado'];
-    $sql = "SELECT nm_cidade as cidade, cd_cidade as codcidade from tb_cidade cid
-    inner join tb_estado est on (cid.id_estado = est.cd_estado)
-    where est.cd_estado = '$estado'
-    order by cid.nm_cidade asc";
-    $result = $mysqli->query($sql); 
-    $cidades = "<option value='' disabled selected>Selecione...</option>";
-    while($row = $result->fetch_object()){
-    	$cidades .= "<option name='cidade' value='".$row->codcidade."'>".$row->cidade."</option>";
+    if(isset($_POST['estado'])){
+        $estado = $_POST['estado'];
+        $sql = "SELECT nm_cidade as cidade, cd_cidade as codcidade from tb_cidade cid
+        inner join tb_estado est on (cid.id_estado = est.cd_estado)
+        where est.cd_estado = '$estado'
+        order by cid.nm_cidade asc";
+        $result = $mysqli->query($sql); 
+        $cidades = "<option value='' disabled selected>Selecione...</option>";
+        while($row = $result->fetch_object()){
+            $cidades .= "<option name='cidade' value='".$row->codcidade."'>".$row->cidade."</option>";
+        }
+        echo $cidades;
     }
-    echo $cidades;
+	if(isset($_POST['senhatual'])){
+        $senha = md5($_POST['senhatual']);
+        $sql = "SELECT * from tb_cliente where ds_senha = '$senha'";
+        $result = $mysqli->query($sql);
+        if($result->num_rows > 0){
+            echo "Senha correspondente <i class='tiny material-icons left'>check</i>";
+        }
+        else{
+            echo "Senha não correspondente <i class='tiny material-icons left'>clear</i>";
+        }
+    }
 ?>
