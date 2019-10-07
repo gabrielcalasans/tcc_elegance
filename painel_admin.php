91d7e29874394035289af74ce2ee52e9bbc53e74
pedrofrati <51917829+pedrofrati@users.noreply.github.com>
2019-10-07 11:18:51 -0300

    Atualizando

--- painel_admin.php
+++ painel_admin.php
@@ -1,54 +1,65 @@
 <?php include('header.php'); ?>
-<meta charset="utf-8">
-	    <style>
+	<meta charset="utf-8">
+	<style>
     	body{
-        background-color: #758DA3; 
-	      }
-	      .mini{
-	      width: 80px;
-	      height: 50px;
-	      }
-	      #logo{
-	        width: 9%;
-	      }
-	      b{
+        	background-color: #758DA3; 
+	    }
+	    .mini{
+	    	width: 80px;
+	    	height: 50px;
+	    }
+	    #logo{
+			width: 9%;
+			transition: 0.5s;
+		}
+		#logo:hover{
+			width: 9.5%;
+		}
+	    b{
 	        font-size: 15px;
-	      }
-	      #but{
+	    }
+	    #but{
 	      	margin-right: 3%;
-	      }
-	      .icon{
-                padding: 5px;
-                width: 20%;
-                color: yellow;
-                transition: 0.4s;
-            }
+	    }
+	    .icon{
+            padding: 5px;
+            width: 20%;
+            color: yellow;
+            transition: 0.4s;
+        }
+        .icon:hover{
+            width: 22%;
+        }
+		#lfooter{
+            color: white;
+            transition: 0.4s;
+        }
 
-            .icon:hover{
-                width: 22%;
-            }
-            footer{
-            	margin-top: 15%;
-            }
+        #lfooter:hover{
+            color: #FFCA37; 
+        }
+        footer{
+        	margin-top: 15%;
+        }
     </style>
 </head>
 <body>
 	<nav class="black darken-2">
     	<div class="nav-wrapper" align="center">
-        <a href="painel_admin.php"><img id="logo" src="images/logotipo2.png"></a>
-      </div>      
+        	<a href="painel_admin.php"><img id="logo" src="images/logotipo2.png"></a>
+      	</div>      
     </nav>
-<center>
-<h1>Área do Administrador</h1>
-<?php 
-	include('conn.php');	
-	$nmfuncionario = $_SESSION['nmadmin'];
-?>
-<h4>Funcionário: <?php echo $nmfuncionario; ?></p></h4>
-<a id="but"href="ver_reserva.php" class="waves-effect waves-light teal darken-3 btn">Visualizar reservas</a>
-<a id="but" href="ver_quarto.php" class="waves-effect waves-light teal darken-3 btn">Visualizar quartos</a>
-<a id="but" href="ver_tipos.php" class="waves-effect waves-light teal darken-3 btn">Visualizar tipos</a>
-<a id="but" href="estatisticas.php" class="waves-effect waves-light teal darken-3 btn">Visualizar estatísticas</a>
-<a id="but" href="addfotos.php" class="waves-effect waves-light teal darken-3 btn">Adicionar fotos</a>
-</center>
+	<center>
+		<h1>Área do Administrador</h1>
+		<?php 
+			include('conn.php');	
+			$nmfuncionario = $_SESSION['nmadmin'];
+		?>
+		<h4>Funcionário: <?php echo $nmfuncionario; ?></p></h4>
+		<a id="but"href="ver_reserva.php" class="waves-effect waves-light indigo darken-3 btn">Visualizar reservas</a>
+		<a id="but" href="ver_quarto.php" class="waves-effect waves-light indigo darken-3 btn">Visualizar quartos</a>
+		<a id="but" href="ver_tipos.php" class="waves-effect waves-light indigo darken-3 btn">Visualizar tipos</a>
+		<a id="but" href="estatisticas.php" class="waves-effect waves-light indigo darken-3 btn">Visualizar estatísticas</a>
+		<a id="but" href="addfotos.php" class="waves-effect waves-light indigo darken-3 btn">Adicionar fotos</a>
+	</center>
 <?php include('footer.php'); ?>
