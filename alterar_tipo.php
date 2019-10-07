e0202dd12e877ff078551da1f3ffa67f6bfbdfde
Gabriel Calasans <48914341+gabrielcalasans@users.noreply.github.com>
2019-10-07 12:17:29 -0300

    Alteração 07-10 (Segunda aula)
    Alterar tipo (falta alterar e resolver problema da imagem)

---
+++ alterar_tipo.php
@@ -0,0 +1,237 @@
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>  
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+ -->
+
+<?php include('conn.php');
+	  date_default_timezone_set('America/Sao_paulo');
+	  //adicionar imagem
+ ?>
+ <?php include('header.php'); ?>
+
+<header>
+	<?php 
+		
+		$cdtipo = $_GET['id'];
+		$consultadados = "SELECT * FROM tb_tipo WHERE cd_tipo = \"$cdtipo\"";
+		$dados = $mysqli->query($consultadados);
+		while($linha = $dados->fetch_object())
+		{	
+			$descricao = $linha->ds_tipo;
+			$nome = $linha->nm_tipo;
+			$valor = $linha->vl_quarto;
+			$endimagem = $linha->ds_imagem;
+		}
+
+
+	?>
+		<title>
+			Alterar tipos de quarto | Elegance
+		</title>	
+
+
+<style type="text/css">
+	#lista
+	{
+		width: 100%;
+		height: 90.5%;
+		position: relative;
+	}
+	fieldset
+	{
+
+		float: left;
+		width: 60%;
+		padding: 10px;
+	}
+	#listagem
+	{
+		width: 20%;
+		height:75.5%;
+		margin-left: 5px;
+	}
+	#func
+	{
+		font-weight: 350;
+		color:white;
+
+	}	
+	#func:active
+	{
+		background-color:#9ccc65;
+	}
+	#imgespaco
+	{
+		width: 100%;
+		height:100%;
+	}
+	#imgcolocada
+	{
+		width: 100%;
+		height: 100%;
+	}
+	#imagem
+	{
+		width: 20%;
+		height: 30%;
+		position: relative;
+		margin-top:-6%;
+	}
+	#formulario
+	{
+
+	}
+	body
+	{
+		padding: 15px;
+	}
+	#return
+	{
+		margin-left:1%;
+		
+	}
+	#return:hover
+	{
+		background-color: #ff8a65; 
+		color:white;
+	}
+	#return:active
+	{
+		background-color: red;
+	}
+	#enviar:active
+	{
+		background-color: lightgreen;
+	}
+	#enviar:hover
+	{
+		background-color: darkgreen;
+	}
+
+
+
+
+
+	
+	
+	
+</style>
+</header>
+<body>
+<form method="POST" enctype="multipart/form-data">
+	<fieldset class="card">
+		Nome do Tipo: <br>
+		<label>Adicione o nome do tipo  </label>
+		<input type="text" value="<?php echo $nome; ?>" required name="tipo"><p>
+		Contém: <br>
+		<label>Adicione o que o tipo de quarto oferece  </label>
+		
+			<input type="text" id="descricao"> <a  class="waves-light btn-small" id="func">Adicionar à descrição<i class="material-icons right">add</i></a><p>	
+		
+		Valor R$:<br>
+		<label>Adicione o valor do tipo do quarto</label>
+			<input class="col s2" type="number" required id="dinheiro" value="<?php echo $valor; ?>" name="dinheiro" class="dinheiro form-control" style="display:inline-block" /><p>
+		Imagem:<br>
+		<label>Adicione fotos na galeria</label>
+	               <div class = "file-field input-field">
+	                  <div  class = "btn-small waves-light yellow darken-2">
+	                     <span>Procurar fotos<i class='material-icons right'>add_to_photos</i></span>
+	                     <input type="file" id="img" value="<?php echo $endimagem; ?>" name="imagem" accept="image/x-png,image/gif,image/jpeg"  />
+	                  </div>
+	                  
+	           
+
+		 <?php include('modal-imagem.php'); ?>
+		<br><br><br><p>
+
+ <button class="btn waves-effect waves-light" type="submit" id="enviar" name="action">Enviar<i class="material-icons right">send</i></button>
+ <a href="ver_tipos.php" id="return" class="btn waves-effect waves-light">Voltar <i class="material-icons right">arrow_back</i></a>
+	</fieldset>
+</fieldset>
+
+	 <fieldset class="card" id="listagem">Descrição:<br>
+	 	<label for="exampleFormControlTextarea1">Adicione ou edite a descrição aqui</label>
+	 		<textarea id="lista" class="form-control" name="lista" id="exampleFormControlTextarea1" rows="3"><?php echo $descricao; ?></textarea>
+
+	 </fieldset>
+
+
+
+</form>
+
+
+
+
+		<script>
+			$(document).ready(function(){
+
+
+				$('#func').click(function(){
+					$('#lista').append('•'+$('#descricao').val()+'<br> ' );	
+					$('#descricao').val('');		
+					
+					
+				});	
+				$('#enviar').click(function(){
+					$('#lista').removeAttr('disabled');
+				})
+
+
+
+
+			});
+		</script>
+
+			</body>
+<?php 
+if(isset($_POST['tipo']))
+{
+	$nome = $_POST['tipo'];
+	$descricao = $_POST['lista'];
+	$valor = $_POST['dinheiro'];	
+	if(isset($_FILES['imagem']))
+		{
+
+		    $extensao = strtolower(substr($_FILES['imagem']['name'], -4)); //pega a extensao do arquivo
+		    if($extensao = "jpeg")
+		    {
+		    	$imagem = time() .".". $extensao; //define o nome do arquivo
+				$diretorio = "images/"; //define o diretorio para onde enviaremos o arquivo
+				move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio.$imagem); //efetua o upload
+
+		    }
+		    else
+		    {
+		    	$imagem = time() . $extensao; //define o nome do arquivo
+			    $diretorio = "images/"; //define o diretorio para onde enviaremos o arquivo
+			    move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio.$imagem); //efetua o upload
+
+		    }
+		   
+	    }
+    else
+	    {
+	    	$imagem = "";
+	    }
+	$sql = "INSERT into tb_tipo VALUES(null,'$nome','$descricao','$valor','$imagem')";
+	if(!$mysqli->query($sql))
+		{
+			echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+		}
+	else
+		{
+			echo "<script>alert('Cadastrado com sucesso!');</script>";
+			echo "<script>window.location.replace('cadastro_tipo.php');</script>";
+		}
+
+}
+
+
+
+
+
+?>
--- ver_tipos.php
+++ ver_tipos.php
@@ -9,14 +9,14 @@
 </style>
 <body>
 	<h1>Ver Tipos</h1>
-	<button><a href="cadastro_tipo.php">Cadastrar tipo</a></button>
+	<button><a href="cadastro_tipo.php">Cadastrar tipo</a></button> <a href="painel_admin.php">Painel de Controle</a>
 	<p>
 
 	<?php 
 		$consultatipo = "SELECT * FROM tb_tipo";
 		$tipo = $mysqli->query($consultatipo);
 		while($linha = $tipo->fetch_object())
-			{
+			{				
 				if($linha->ds_imagem!="")
 				{
 					echo '<fieldset>Tipo: '.$linha->nm_tipo.'<p> Descrição: '.$linha->ds_tipo.'<br>Valor: '.$linha->vl_quarto.'<p> <img id="imgtipo" src="images/'.$linha->ds_imagem.'">';
@@ -25,9 +25,9 @@
 				{
 					echo '<fieldset>Tipo: '.$linha->nm_tipo.'<p> Descrição: '.$linha->ds_tipo.'<br>Valor: '.$linha->vl_quarto.' dsadasdsa';
 				}
-
-				echo '<p><button><a href="alterar_tipo.php?id='.$linha->cd_tipo.'">Alterar</a></button> <button><a href="ver_tipos.php?id='.$linha->cd_tipo.'">Excluir</a></button></fieldset><br>';
-
+				
+					echo '<p><button><a href="alterar_tipo.php?id='.$linha->cd_tipo.'">Alterar</a></button> <button><a href="ver_tipos.php?id='.$linha->cd_tipo.'">Excluir</a></button></fieldset><br>';
+											
 			}
 	if(isset($_GET['id']))
       {
@@ -36,7 +36,7 @@
         $deletar_tipo="DELETE FROM tb_tipo WHERE cd_tipo=\"$codtipo\"";
         if(!$mysqli->query($deletar_tipo))
           {
-            echo "<script>alert('Não é possível excluir!!');
+            echo "<script>alert('Não é possível excluir!! Há um quarto cadastrado com esse tipo!!');
            		  window.location.href='ver_tipos.php';                   
                </script>";
           }
