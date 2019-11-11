55e02d6e00dda1d0f71b5b50fdd1261e37cf8857
pedrofrati <51917829+pedrofrati@users.noreply.github.com>
2019-11-11 09:21:23 -0300

    Testando

--- testequarto.php
+++ testequarto.php
@@ -21,6 +21,37 @@
 				width: 9.5%;
 			}
 
+			.input-field input:focus + label {
+			    color: #283593 !important;
+			}
+
+			.input-field input:focus {
+			    border-bottom: 1px solid #283593 !important;
+			    box-shadow: 0 1px 0 0 #283593 !important;
+			}
+
+			[type="radio"]:checked + span:after,
+			[type="radio"].with-gap:checked + span:before,
+			[type="radio"].with-gap:checked + span:after {
+			  border: 2px solid #283593;
+			}
+
+			[type="radio"]:checked + span:after,
+			[type="radio"].with-gap:checked + span:after {
+			  background-color: #283593;
+			}
+
+			.dropdown-content li > a, .dropdown-content li > span {
+				color: #283593 !important;
+			}
+
+            .input-field textarea:focus + label {
+                color: #283593 !important;
+            }
+            .input-field textarea:focus {
+                border-bottom: 1px solid #283593 !important;
+                box-shadow: 0 1px 0 0 #283593 !important
+            }
 	    </style>
 	</head>
 	<body>
@@ -42,12 +73,13 @@
     						<input id="numero" type="number" name="numeroquarto" required="" class="validate"><label for="numero">Número do Quarto </label>
     					</div>
     					<div class="input-field col s6 m6">
-    						<select name="status" id="status">
-								<option value="" disabled selected>Status</option>
-								<option value="1">Disponível</option>
-								<option value="2">Não disponível</option>
-							</select><label for="status">Status do Quarto</label>
-    					</div>
+					    	<select>
+					      		<option value="" disabled selected></option>
+					      		<option value="1">Disponível</option>
+					      		<option value="2">Não disponível</option>
+					    	</select>
+					    	<label>Status do quarto</label>
+					  	</div>
     				</div>
     				<div class="row">
     					<div class="input-field col s12 m12">
@@ -57,22 +89,67 @@
     				</div>
     				<div class="row">
     					<div class="col s4 m4">
-					    	<div class="card">
-					    		<div class="card-image">
-					          		<img id="foto" src="images/x.png">
-					          		<span class="card-title">Tipo do Quarto</span>
-					        	</div>
-					        	<div class="card-content">
-					          		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ante ante, bibendum nec turpis tristique, fringilla luctus tellus.</p>
-					        	</div>
-					        	<div class="card-action">
-					          		<input type="radio" name="tipoquarto">
-					        	</div>
-					      	</div>
+    						<label for="1">
+						    	<div class="card">
+						    		<div class="card-image">
+						          		<img id="foto" src="images/x.png">
+						        	</div>
+						        	<div class="card-content">
+						          		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ante ante, bibendum nec turpis tristique, fringilla luctus tellus.</p>
+						        	</div>
+						        	<div class="card-action">
+						          		<label>
+									        <input name="tipoquarto" type="radio" id="1" />
+									    	<span>Tipo</span>
+									    </label>
+						        	</div>
+						      	</div>
+					      	</label>
+					    </div>
+					    <div class="col s4 m4">
+    						<label for="2">
+						    	<div class="card">
+						    		<div class="card-image">
+						          		<img id="foto" src="images/x.png">
+						        	</div>
+						        	<div class="card-content">
+						          		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ante ante, bibendum nec turpis tristique, fringilla luctus tellus.</p>
+						        	</div>
+						        	<div class="card-action">
+						          		<label>
+									        <input name="tipoquarto" type="radio" id="2" />
+									    	<span>Tipo</span>
+									    </label>
+						        	</div>
+						      	</div>
+					      	</label>
+					    </div>
+					    <div class="col s4 m4">
+    						<label for="3">
+						    	<div class="card">
+						    		<div class="card-image">
+						          		<img id="foto" src="images/x.png">
+						        	</div>
+						        	<div class="card-content">
+						          		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ante ante, bibendum nec turpis tristique, fringilla luctus tellus.</p>
+						        	</div>
+						        	<div class="card-action">
+						          		<label>
+									        <input name="tipoquarto" type="radio" id="3" />
+									    	<span>Tipo</span>
+									    </label>
+						        	</div>
+						      	</div>
+					      	</label>
 					    </div>
     				</div>
     			</form>
     		</div>
     	</div>
+    	<script>
+			$(document).ready(function(){
+			    $('select').formSelect();
+			});
+		</script>
 	</body>
 </html>
