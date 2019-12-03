be23a340e0ec9b1812ec2d4b86dbcd202df66d3b
Igor Oliveira <45038664+igoretec@users.noreply.github.com>
2019-12-03 11:42:21 -0300

    Add files via upload

--- ficha.php
+++ ficha.php
@@ -9,18 +9,62 @@
 			<meta charset="utf-8">
 			<title>Ficha cadastral</title>
 		</head>
-		<body>
-			<table border="3">
-				<tr>
-					<td>ihuuu</td>
-					<td>aaaaaaaaaa</td>
-				</tr>
-			</table>
+		<body style="font-family: helvetica;">
+			<div class="row">
+				<div align="center" class="col s12">
+					<img src="images/logotipo3.png" style="width: 50%;">
+				</div>
+			</div>
+			<div class="row">
+				<div align="center" class="col s12">
+					Av. Presidente Vargas, 1001. Centro. Itanhaém - São Paulo.<br>
+					Telefones: (13) 99862-3243 - VIVO / 98116-1914 - TIM.
+				</div>
+			</div>
+			<div class="row">
+				<table>
+					<tr>
+						<td class="col s12"><b>Nome Completo:*</b></td>
+					</tr>
+					<tr>
+						<td class="col s6"><b>Profissão:*</b></td>
+						<td class="col s6"><b>E-mail:*</b></td>
+					</tr>
+					<tr>
+						<td class="col s4"><b>CPF:*</b></td>
+						<td class="col s4"><b>RG:*</b></td>
+						<td class="col s4"><b>Órgão Exp.:*</b></td>
+					</tr>
+					<tr>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+					</tr>
+					<tr>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+					</tr>
+					<tr>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+					</tr>
+					<tr>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+						<td class="col s4">lalalallaal: </td>
+					</tr>
+				</table>
+				<div align="center" class="col s12">
+					
+				</div>
+			</div>
 		</body>
 	</html>';
 	$arquivo = "Ficha_cadastral.pdf";
 	$mpdf = new mPDF(); 
-	$css = file_get_contents("css/pdf.css");
+	$css = file_get_contents("https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css");
 	$mpdf->WriteHTML($css,1);
 	$mpdf->WriteHTML($html);
 	$mpdf->Output($arquivo, 'I');
