6e92cd31a604375aefa024c08334c40d1eb2ff3e
pedrofrati <51917829+pedrofrati@users.noreply.github.com>
2019-12-06 09:24:22 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -82,6 +82,15 @@
             .dropdown-content li > a, .dropdown-content li > span {
                 color: #fbc02d !important;
             }
+
+            @font-face {
+                font-family: "Lobster";
+                src: url("fonts/lobster/Lobster.otf") format("truetype");
+            }
+
+            .lobster-font{
+                font-family: "Lobster"; 
+            }
         </style>
     </head>
 	<body>
@@ -135,7 +144,7 @@
         </div>
         <center>
         <div id="i1" class="section">
-            <h4>Inclusos</h4>
+            <h4 class="lobster-font">Inclusos</h4>
             <br>
             <div class="row">
                 <div class="col m4">
@@ -175,7 +184,7 @@
         </div>
         <div class="divider"></div>
         <div class="section">
-            <h4>Depoimentos</h4>
+            <h4 class="lobster-font">Depoimentos</h4>
             <div class="row">
                 <div class="col s12">
                     <div class="carousel carousel-slider center">
